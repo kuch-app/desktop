711acc885c411d990c20db16fc9801252a3da0e9
Varga Roland <106912409+vargaRolandGit@users.noreply.github.com>
2024-12-18 12:07:11 +0100

    0.1.0a

--- kuch-desktop/src/renderer/src/App.jsx
+++ kuch-desktop/src/renderer/src/App.jsx
@@ -1,35 +1,193 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { TOTP } from 'totp-generator';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
+import "./assets/main.css"
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+    const [secretKey, setSecretKey] = useState('');
+    const [codeName, setCodeName] = useState('');
+    const [codes, setCodes] = useState([]);
+    const [isDeleteModalActive, setIsDeleteModalActive] = useState(false);
+    const [isSettingsModalActive, setIsSettingsModalActive] = useState(false);
+    const [codeToDelete, setCodeToDelete] = useState(null);
+    const [codeForSettings, setCodeForSettings] = useState(null);
 
-  return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
+    const generateOtp = (key) => {
+        const { otp } = TOTP.generate(key);
+        return otp;
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCodes(prevCodes =>
+                prevCodes.map(code => ({
+                    ...code,
+                    otp: generateOtp(code.secretKey),
+                }))
+            );
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const saveSecretKeyToCookies = () => {
+        if (!secretKey || !codeName) return; // Don't save if inputs are empty
+        const newCode = { name: codeName, secretKey, otp: generateOtp(secretKey) };
+        const updatedCodes = [...codes, newCode];
+        Cookies.set('codes', JSON.stringify(updatedCodes), { expires: 7 });
+        setCodes(updatedCodes);
+        setSecretKey(''); // Reset the secret key input
+        setCodeName(''); // Reset the code name input
+    };
+
+    const openDeleteModal = (code) => {
+        setCodeToDelete(code);
+        setIsDeleteModalActive(true);
+    };
+
+    const confirmDelete = () => {
+        if (codeToDelete) {
+            const updatedCodes = codes.filter(code => code.name !== codeToDelete.name);
+            Cookies.set('codes', JSON.stringify(updatedCodes), { expires: 7 });
+            setCodes(updatedCodes);
+        }
+        setIsDeleteModalActive(false);
+        setCodeToDelete(null);
+    };
+
+    const cancelDelete = () => {
+        setIsDeleteModalActive(false);
+        setCodeToDelete(null);
+    };
+
+    const openSettingsModal = (code) => {
+        setCodeForSettings(code);
+        setIsSettingsModalActive(true);
+    };
+
+    const closeSettingsModal = () => {
+        setIsSettingsModalActive(false);
+        setCodeForSettings(null);
+    };
+
+    useEffect(() => {
+        const savedCodes = Cookies.get('codes');
+        if (savedCodes) {
+            setCodes(JSON.parse(savedCodes));
+        }
+    }, []);
+
+    const [visible, setVisible] = useState(true);
+    return (
+        <div>
+            <div className="webapp">
+                <div className="input-fields">
+                    <div>
+                        <input
+                            type="text"
+                            value={codeName}
+                            onChange={(e) => setCodeName(e.target.value)}
+                            placeholder="fiók neve"
+                        />
+                    </div>
+
+                    <div>
+                        <input
+                            type="text"
+                            value={secretKey}
+                            onChange={(e) => setSecretKey(e.target.value)}
+                            placeholder="azonosító"
+                        />
+                    </div>
+                    <button className="btn btn-primary" onClick={saveSecretKeyToCookies}>
+                        Fiók Hozzáadása
+                    </button>
+                </div>
+                <div className="divider text-center" data-content="KÓDJAI"></div>
+                <div>
+                    {codes.length > 0 ? (
+                        <ul className="card-sect">
+                            {codes.map((code, index) => (
+                                <li key={index}>
+                                    <div className="card cardelem">
+                                        <h2>{code.otp}</h2>
+                                        <p className="text-small"><i class="icon icon-people"></i> {code.name}</p>
+                                        <div className="buttons">
+                                            <button
+                                                className="btn my"
+                                                onClick={() => openSettingsModal(code)}
+                                            >
+                                                Beállítások
+                                            </button>
+                                            <button
+                                                className="btn btn-error"
+                                                onClick={() => openDeleteModal(code)}
+                                            ><i class="icon icon-delete"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <div class="empty">
+                          <div class="empty-icon">
+                            <i class="icon icon-people icon-2x"></i>
+                          </div>
+                          <p class="empty-title h5">Önnek nincsenek kódjai!</p>
+                          <p class="empty-subtitle">A fenti felületen adhat hozzá kódokat.</p>
+                          <div class="empty-action">
+                          </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            {/* Delete Modal */}
+            {isDeleteModalActive && (
+                <div className="modal active">
+                    <a href="#close" className="modal-overlay" aria-label="Close" onClick={cancelDelete}></a>
+                    <div className="modal-container">
+                        <div className="modal-header">
+                            <a href="#close" className="btn btn-clear float-right" aria-label="Close" onClick={cancelDelete}></a>
+                            <div className="modal-title h5">Törlés megerősítése</div>
+                        </div>
+                        <div className="modal-body">
+                            <div className="content">
+                                Biztosan törölni szeretné a(z) "{codeToDelete?.name}" kódot?
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn btn-error my" onClick={confirmDelete}>Törlés</button>
+                            <button className="btn btn-secondary" onClick={cancelDelete}>Mégse</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {/* Settings Modal */}
+            {isSettingsModalActive && (
+                <div className="modal active">
+                    <a href="#close" className="modal-overlay" aria-label="Close" onClick={closeSettingsModal}></a>
+                    <div className="modal-container">
+                        <div className="modal-header">
+                            <a href="#close" className="btn btn-clear float-right" aria-label="Close" onClick={closeSettingsModal}></a>
+                            <div className="modal-title h5">Fiók Beállításai</div>
+                        </div>
+                        <div className="modal-body">
+                            <div className="content">
+                                <p><strong>Fiók neve:</strong> {codeForSettings?.name}</p>
+                                <p><strong>Azonosító:</strong> {codeForSettings?.secretKey}</p>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn btn-secondary" onClick={closeSettingsModal}>Bezárás</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
+    );
 }
 
-export default App
-
+export default App;
